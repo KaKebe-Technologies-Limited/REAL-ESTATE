dbe712c7c0d36678c1493d29ff61d9804c85a2eb
nyayo <ivannyayo@gmail.com>
2025-04-11 10:49:16 +0300

    updated

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 // define('DB_NAME', value: 'allea');
 define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'u850523537_Allea');
-define('DB_PASSWORD', '@Allea#1234');
+define('DB_PASSWORD', 'Admin@101619#');
 define('DB_NAME', 'u850523537_Allea');
 
 ?>
